92dc2a82c6e79bbf1eba05566a93d9f7c8cbf513
kkk7373 <kantaizhongcun342@gmail.com>
2025-10-29 23:36:54 +0900

    mod

--- src/app/api/checkout_sessions/route.ts
+++ src/app/api/checkout_sessions/route.ts
@@ -1,7 +1,15 @@
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
+
+if (!stripeSecretKey) {
+  throw new Error(
+    "STRIPE_SECRET_KEY is not defined. Please check your .env file or environment variables."
+  );
+}
+
+const stripe = new Stripe(stripeSecretKey);
 export async function POST(req: Request) {
   const { title, price, bookId, userId } = await req.json();
   try {
--- src/app/api/checkout_sessions/success/route.ts
+++ src/app/api/checkout_sessions/success/route.ts
@@ -4,7 +4,15 @@ import prisma from "@/app/lib/prisma";
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
+
+if (!stripeSecretKey) {
+  throw new Error(
+    "STRIPE_SECRET_KEY is not defined. Please check your .env file or environment variables."
+  );
+}
+
+const stripe = new Stripe(stripeSecretKey);
 export async function POST(request: Request) {
   const { sessionId } = await request.json();
 
