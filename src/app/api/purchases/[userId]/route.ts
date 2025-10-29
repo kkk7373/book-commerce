461d073116fc68f5a80dd6f10471120b3bcbc973
kkk7373 <kantaizhongcun342@gmail.com>
2025-10-29 23:01:04 +0900

    mod

--- src/app/api/purchases/[userId]/route.ts
+++ src/app/api/purchases/[userId]/route.ts
@@ -1,12 +1,12 @@
-import { NextResponse } from "next/server";
+import { NextResponse, type NextRequest } from "next/server";
 import prisma from "@/app/lib/prisma";
 
 //特定ユーザーの購入履歴を取得
 export async function GET(
-  request: Request,
-  { params }: { params: { userId: string } }
+  request: NextRequest,
+  context: { params: { userId: string } }
 ) {
-  const { userId } = params;
+  const { userId } = context.params;
   try {
     const purchases = await prisma.purchase.findMany({
       where: {
