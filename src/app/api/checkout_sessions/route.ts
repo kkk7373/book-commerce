96d00c90f32ea9ca1a1342e7b4fba968cbb366fe
kkk7373 <kantaizhongcun342@gmail.com>
2025-10-30 22:22:37 +0900

    return_url mod

--- src/app/api/checkout_sessions/route.ts
+++ src/app/api/checkout_sessions/route.ts
@@ -33,8 +33,8 @@ export async function POST(req: Request) {
         },
       ],
       mode: "payment",
-      success_url: `http://localhost:3000/book/checkout-success?session_id={CHECKOUT_SESSION_ID}`,
-      cancel_url: `http://localhost:3000/`,
+      success_url: `${process.env.NEXT_BASE_URL}/book/checkout-success?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: `${process.env.NEXT_BASE_URL}/`,
     });
     return NextResponse.json({ checkoutURL: session.url });
   } catch (err: any) {
